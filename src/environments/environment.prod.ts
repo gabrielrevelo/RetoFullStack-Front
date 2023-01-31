55d133391c0b7843cf5a3077baa50a0d57bcdebd
GaRev-Dev <revelo.gabriel.38@gmail.com>
2023-01-31 17:46:15 -0500

    Conexion Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyBNJi20rFfv81no5l99AFImQS20_a0_LCg",
-    authDomain: "fir-83199.firebaseapp.com",
-    projectId: "fir-83199",
-    storageBucket: "fir-83199.appspot.com",
-    messagingSenderId: "1097598825117",
-    appId: "1:1097598825117:web:0b5b4b34c8e1abc3815a74"
+    projectId: 'naves-firebase',
+    appId: '1:725512081199:web:2e40efd2bea8b6d135dbda',
+    storageBucket: 'naves-firebase.appspot.com',
+    apiKey: 'AIzaSyAJZ57TIuJWBPYhAZQbBpdAC81LRDDldEo',
+    authDomain: 'naves-firebase.firebaseapp.com',
+    messagingSenderId: '725512081199',
   }
 
 };
